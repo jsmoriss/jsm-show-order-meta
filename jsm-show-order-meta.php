a383047902c0c1ef4f72dc6366f959ecf46ebf10
jsmoriss <jsm@surniaulula.com>
2026-07-20 04:18:28 +0000

    v4.9.0

--- jsm-show-order-meta.php
+++ jsm-show-order-meta.php
@@ -12,7 +12,7 @@
  * Description: Show WooCommerce order metadata in a metabox when editing HPOS orders - a great tool for debugging issues with HPOS order metadata.
  * Requires PHP: 7.4.33
  * Requires At Least: 6.0
- * Tested Up To: 7.0
+ * Tested Up To: 7.0.2
  * Version: 4.9.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
