bfade7016d62732c51f96e1b653d017fb3ec943e
jsmoriss <jsm@surniaulula.com>
2026-09-12 14:34:51 +0000

    v4.9.1-rc.1

--- lib/order.php
+++ lib/order.php
@@ -141,7 +141,7 @@ if ( ! class_exists( 'JsmSomOrder' ) ) {
 
 			$metabox_id   = 'jsmsom';
 			$obj_id       = SucomUtil::sanitize_int( $_POST[ 'obj_id' ] );		// Returns integer or null.
-			$meta_key     = SucomUtil::sanitize_meta_key( $_POST[ 'meta_key' ] );	// Decode/remove html/js/css and quotes.
+			$meta_key     = SucomUtil::sanitize_meta_key( $_POST[ 'meta_key' ] );	// Decode and remove html/js/css and quotes.
 			$table_row_id = SucomUtil::sanitize_key( $metabox_id . '_' . $obj_id . '_' . $meta_key );
 			$order_obj    = wc_get_order( $obj_id );
 			$delete_cap   = apply_filters( 'jsmsom_delete_meta_capability', 'manage_options', $order_obj );
